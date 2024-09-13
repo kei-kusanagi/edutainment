676cb51404340ba16a5939dbaac26c7bc24c8f7e
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-13 15:22:35 -0600

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Juan Carlos Robledo Morales on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
